70b256066f0c95f67418944c833db9739afb43b9
Umut-Polat-GM <umut.polat.272@gmail.com>
2024-03-11 16:17:55 +0300

    feat: authMiddleware update and added main route

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,23 +1,24 @@
-const jwt = require('jsonwebtoken')
-const { UnauthenticatedError } = require('../errors')
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
 
 const authenticationMiddleware = async (req, res, next) => {
-  const authHeader = req.headers.authorization
+    const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new UnauthenticatedError('No token provided')
-  }
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        throw new UnauthenticatedError("No token provided");
+    }
 
-  const token = authHeader.split(' ')[1]
+    const token = authHeader.split(" ")[1];
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    const { username } = decoded
-    req.user = { username }
-    next()
-  } catch (error) {
-    throw new UnauthenticatedError('Not authorized to access this route')
-  }
-}
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const { userId, username } = decoded;
+        req.user = { userId, username };
+        next();
+    } catch (error) {
+        throw new UnauthenticatedError("Not authorized to access this route");
+    }
+};
 
-module.exports = authenticationMiddleware
+module.exports = authenticationMiddleware;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,11 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const login = require("../controllers/login");
-const inventory = require("../controllers/inventory");
+const register = require("../controllers/register");
+const {
+    getAllInventories,
+    getInventory,
+    createInventory,
+    updateInventory,
+    deleteInventory,
+} = require("../controllers/inventory");
 
-const authMiddleware = require('../middleware/authentication')
+const authMiddleware = require("../middleware/authentication");
 
 router.route("/login").post(login);
-router.route("/inventory").get(authMiddleware, inventory);
+router.route("/register").post(register);
+router.route("/inventory").get(authMiddleware, getAllInventories).post(authMiddleware, createInventory);
+router.route("/inventory/:id").get(authMiddleware, getInventory).patch(authMiddleware, updateInventory).delete(authMiddleware, deleteInventory);
 
 module.exports = router;
