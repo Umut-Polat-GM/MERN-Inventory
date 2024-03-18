9d1284f0130012943c5198a2e87c5da9df28ab22
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2024-03-18 08:58:53 +0300

    feat: error side improved

--- server/errors/index.js
+++ server/errors/index.js
@@ -1,9 +1,11 @@
-const CustomAPIError = require('./custom-error')
-const BadRequestError = require('./bad-request')
-const UnauthenticatedError = require('./unauthenticated')
+const CustomAPIError = require("./custom-error");
+const BadRequestError = require("./bad-request");
+const UnauthenticatedError = require("./unauthenticated");
+const NotFoundError = require("./not-found");
 
 module.exports = {
-  CustomAPIError,
-  BadRequestError,
-  UnauthenticatedError,
-}
+    CustomAPIError,
+    BadRequestError,
+    UnauthenticatedError,
+    NotFoundError,
+};
