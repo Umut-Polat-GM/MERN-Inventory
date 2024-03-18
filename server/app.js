bfbce509da7b79a42307e2e2795c1963d31e665c
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2024-03-19 00:49:06 +0300

    feat: add extra security packages

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
 
+//extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -11,7 +17,17 @@ const mainRouter = require("./routes/routes");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+    rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100,
+    })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use("/api/v1", mainRouter);
 
