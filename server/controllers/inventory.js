cbc686c900c30d2b4e7d57c04e44c4a936e6b486
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2024-03-18 08:59:11 +0300

    feat: inventory controller update

--- server/controllers/inventory.js
+++ server/controllers/inventory.js
@@ -1,13 +1,17 @@
 const Inventory = require("../models/Inventory");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, UnauthenticatedError } = require("../errors");
+const { BadRequestError, UnauthenticatedError, NotFoundError } = require("../errors");
 
 const getAllInventories = async (req, res) => {
     const inventories = await Inventory.find({ createdBy: req.user.userId }).sort("createdAt");
     res.status(StatusCodes.OK).json({ inventories, count: inventories.length });
 };
 const getInventory = async (req, res) => {
-    res.status(200).json({ msg: "getInventory" });
+    const inventory = await Inventory.findOne({ _id: req.params.id, createdBy: req.user.userId });
+    if (!inventory) {
+        throw new BadRequestError("Inventory does not exist");
+    }
+    res.status(StatusCodes.OK).json({ inventory });
 };
 
 const createInventory = async (req, res) => {
@@ -17,11 +21,37 @@ const createInventory = async (req, res) => {
 };
 
 const updateInventory = async (req, res) => {
-    res.status(200).json({ msg: "updateInventory" });
+    const { id } = req.params;
+    const { name, price, quantity, description, responsible } = req.body;
+    if (
+        name === "" ||
+        price === "" ||
+        quantity === "" ||
+        description === "" ||
+        responsible === ""
+    ) {
+        throw new BadRequestError(
+            "name, price, quantity, description, responsible fields are required"
+        );
+    }
+    const inventory = await Inventory.findOneAndUpdate(
+        { _id: id, createdBy: req.user.userId },
+        req.body,
+        { new: true, runValidators: true }
+    );
+    if (!inventory) {
+        throw new UnauthenticatedError("Inventory does not exist or you are not authorized");
+    }
+    res.status(StatusCodes.OK).json({ inventory });
 };
 
 const deleteInventory = async (req, res) => {
-    res.status(200).json({ msg: "deleteInventory" });
+    const { id } = req.params;
+    const inventory = await Inventory.findByIdAndDelete({ _id: id, createdBy: req.user.userId });
+    if (!inventory) {
+        throw new NotFoundError("Inventory does not exist or you are not authorized");
+    }
+    res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
